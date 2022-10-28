ae05d5b799bd108e390748e3b8020f10af6e8463
niulin lnc <admin@niulin.net>
2022-10-28 10:09:27 +0800

    翻译微调

--- site/src/components/WorkspaceStats/WorkspaceStats.tsx
+++ site/src/components/WorkspaceStats/WorkspaceStats.tsx
@@ -14,10 +14,10 @@ const Language = {
   templateLabel: "模板",
   statusLabel: "工作区状态",
   versionLabel: "版本",
-  lastBuiltLabel: "最后创建",
+  lastBuiltLabel: "最近创建",
   outdated: "过时",
   upToDate: "最新",
-  byLabel: "最后创建者",
+  byLabel: "创建者",
 }
 
 export interface WorkspaceStatsProps {
