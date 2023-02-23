9f684213153efbb69187e552151d781e7346a2bb
niulin lnc <admin@niulin.net>
2023-02-23 09:30:17 +0800

    增加中文翻译包

---
+++ site/src/i18n/cn/index.ts
@@ -0,0 +1,45 @@
+import auditLog from "./auditLog.json"
+import common from "./common.json"
+import createWorkspacePage from "./createWorkspacePage.json"
+import templatePage from "./templatePage.json"
+import templatesPage from "./templatesPage.json"
+import workspacePage from "./workspacePage.json"
+import agent from "./agent.json"
+import buildPage from "./buildPage.json"
+import workspacesPage from "./workspacesPage.json"
+import usersPage from "./usersPage.json"
+import templateSettingsPage from "./templateSettingsPage.json"
+import templateVersionPage from "./templateVersionPage.json"
+import loginPage from "./loginPage.json"
+import workspaceBuildParametersPage from "./workspaceBuildParametersPage.json"
+import workspaceChangeVersionPage from "./workspaceChangeVersionPage.json"
+import workspaceSchedulePage from "./workspaceSchedulePage.json"
+import appearanceSettings from "./appearanceSettings.json"
+import starterTemplatesPage from "./starterTemplatesPage.json"
+import starterTemplatePage from "./starterTemplatePage.json"
+import createTemplatePage from "./createTemplatePage.json"
+import userSettingsPage from "./userSettingsPage.json"
+
+export const en = {
+  common,
+  workspacePage,
+  auditLog,
+  templatePage,
+  templatesPage,
+  createWorkspacePage,
+  agent,
+  buildPage,
+  workspacesPage,
+  usersPage,
+  templateSettingsPage,
+  templateVersionPage,
+  loginPage,
+  workspaceBuildParametersPage,
+  workspaceChangeVersionPage,
+  workspaceSchedulePage,
+  appearanceSettings,
+  starterTemplatesPage,
+  starterTemplatePage,
+  createTemplatePage,
+  userSettingsPage,
+}
--- site/src/i18n/i18n.ts
+++ site/src/i18n/i18n.ts
@@ -1,15 +1,16 @@
 import i18next from "i18next"
 import { initReactI18next } from "react-i18next"
 import { en } from "./en"
+import { cn } from "./cn"
 
 export const defaultNS = "common"
-export const resources = { en } as const
+export const resources = { cn } as const
 
 export const i18n = i18next.use(initReactI18next)
 
 i18n
   .init({
-    fallbackLng: "en",
+    fallbackLng: "cn",
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
